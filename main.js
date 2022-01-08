c2c5a9669f218f005b3d88be20dac44bef84870e
ChandrakantMishra <78901043+ChandrakantMishra@users.noreply.github.com>
2022-01-08 14:39:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+leftwristx=0;
+rightwristx=0;
+difference=0;
+
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500); 
+
+    canvas = createCanvas(550,500);
+    canvas.position(550,150);
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",GotPoses);
+}
+
+function draw(){
+    background("black");
+    textSize(10);
+    fill('#FFE787');
+    text('Peter',50,400);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+function GotPoses(results){
+    if(results.length > 0){
+    console.log(results);
+    leftwristx = results[0].pose.leftWrist.x;
+    rightwristx = results[0].pose.rightWrist.x;
+    difference= floor(leftwristx-rightwristx);
+    console.log("leftwristx = "+leftwristx+"rightwristx = "+rightwristx+"difference = "+difference);
+    document.getElementById("square_sides").innerHTML="Width and height of the square is = "+difference+"px";
+    }
+}
